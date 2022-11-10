529a450749cfad6d4d9d426660813d17c38bb105
Ashutosh Singh <su.ashutosh@gmail.com>
2022-11-10 16:44:19 +0530

    Day 1

---
+++ recursion/recursion1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    if (n == 0) return 1;
+    return n * fact(n - 1);
+}
+
+int power(int n) {
+    if (n == 0) return 1;
+    return 2 * power(n - 1);
+}
+
+void print0(int n) {
+    if (n == 0) return;
+    cout << n << endl;
+    print0(n - 1);
+}
+
+void print(int n) {
+    if (n == 0) return;
+    print(n - 1);
+    cout << n << endl;
+}
+
+// recursion-1
+
+int main(void) {
+    int n;
+    cin >> n;
+    // cout << fact(n) << endl;
+    // cout << power(n) << endl;
+    print(n);
+    return 0;
+}
